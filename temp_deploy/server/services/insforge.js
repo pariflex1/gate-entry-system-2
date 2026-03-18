69a13da93cb380668acdd9f2200f88d0c23ce518
pariflex1 <dhanistadigital@gmail.com>
2026-03-18 09:18:01 +0530

    Remove temp_deploy to avoid workspace issues

--- temp_deploy/server/services/insforge.js
+++
@@ -1,8 +0,0 @@
-const { createClient } = require('@insforge/sdk');
-
-const insforge = createClient({
-  baseUrl: process.env.INSFORGE_URL,
-  anonKey: process.env.INSFORGE_SERVICE_KEY
-});
-
-module.exports = insforge;
--- temp_deploy/server/static/client/registerSW.js
+++
@@ -1,1 +0,0 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/client/sw.js', { scope: '/client/' })})}
